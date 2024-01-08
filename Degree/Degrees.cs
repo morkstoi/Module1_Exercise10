09181b371925e12a31c8206f1348d94c3b9a0353
theac <theac@DESKTOP-LEARA2L>
2024-01-08 18:30:27 +0300

    Add project files.

---
+++ Degree/Degrees.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Degree
+{
+    class Degrees
+    {
+        private int gradus;
+        private int min;
+        private int sec;
+
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                if (value >= 0 && value <=360)
+                {
+                    gradus = value; 
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(nameof(gradus), "Значение градуса должно быть в диапазоне [0°;360°].");
+                }
+            }
+        }
+        public int Minutes
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if(value >= 0 && value<60)
+                {
+                    min = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(nameof(min), "Значение градуса должно быть в диапазоне [0°;60°).");
+                }
+
+            }
+        }
+        public int Seconds
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                if (value >= 0 && value < 60)
+                {
+                    sec = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(nameof(sec), "Значение градуса должно быть в диапазоне [0°;60°).");
+                }
+
+            }
+        }
+        public Degrees (int gradus, int min, int sec)
+        {
+            Gradus = gradus;
+            Minutes = min;
+            Seconds = sec;
+        }
+        public double ToRadians()
+        {
+            double totalDegrees = gradus + (min/60)+(sec/3600);
+            return totalDegrees * Math.PI / 180;
+        }
+
+
+    }
+}
---
+++ Degree/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Degree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите значение угла в °:");
+                int gradus = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите значение угла в ':");
+                int min = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите значение угла в '':");
+                int sec = Convert.ToInt32(Console.ReadLine());
+                Degrees degrees = new Degrees(gradus, min, sec);
+                double radians = degrees.ToRadians();
+                Console.WriteLine("Угол в радианах: {0}", radians);
+                Console.ReadKey();
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+
+        }
+    }
+}
